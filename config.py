c07c49eaab27f6b42e714882a5507b523714ac4e
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2022-04-12 00:07:03 +0300

    Change std epochs

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch # noqa
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 latent_dim_size = 128
 total_steps = 6
-epochs = [25000 for _ in range(total_steps)]
+epochs = [50000 for _ in range(total_steps)]
 batch_size = [4 for _ in range(total_steps)]
 initial_step = 1
 n_critic = 1
