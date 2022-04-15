e163e653ca6d70821d54fc3693a75fff0d99b666
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2022-04-15 18:59:39 +0300

    Fix gif creation on large images

--- gif_creation/create_gif.py
+++ gif_creation/create_gif.py
@@ -2,21 +2,26 @@ import imageio  # noqa
 from PIL import Image # noqa
 import numpy as np # noqa
 import os
+from alive_progress import alive_bar  # noqa
 
 images = []
-directory = "../run_2022-04-06_22-21/gif/"
+directory = "../gif/"
 
+Image.MAX_IMAGE_PIXELS = 100000000000
 
-for i, filename in enumerate(sorted(os.listdir(directory), key=lambda x: (len(x), x))):
-    if i % 2 == 0:
-        continue
+with alive_bar(len(os.listdir(directory)), title='Processing images') as bar:
+	for i, filename in enumerate(sorted(os.listdir(directory), key=lambda x: (len(x), x))):
+		if i % 4 == 0:
+			continue
 
-    image = Image.open(directory + filename)
-    image = image.convert('RGB')
+		image = Image.open(directory + filename)
+		image = image.convert('RGB')
 
-    pixels = np.asarray(image)
-    pixels = Image.fromarray(pixels)
-    image1 = pixels.resize((800, 800))
-    images.append(image1)
+		pixels = np.asarray(image)
+		pixels = Image.fromarray(pixels)
+		image1 = pixels.resize((800, 800))
+		images.append(image1)
+
+		bar()
 
 imageio.mimsave('learning_process.gif', images)
